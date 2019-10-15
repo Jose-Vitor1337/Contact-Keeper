98eea2d7220314a3b9d93f4713901bc33017db50
Jose-Vitor1337 <josevitordemelo@yahoo.com>
2019-10-14 21:03:17 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var database_1 = require("./config/database");
 var common_1 = require("./common/common");
+var cors_1 = require("cors");
 var express = require("express");
 var app = express();
 app.get('/', function (req, res) { return res.json({ msg: "Welcome to the Contact Keeper API, in the BackEnd" }); });
@@ -13,5 +14,6 @@ app.use(express.json());
 app.use('/api/users', require('./router/users'));
 app.use('/api/authentication', require('./router/authentication'));
 app.use('/api/contacts', require('./router/contacts'));
+app.use(cors_1.default());
 // Creating a local server
 app.listen(common_1.default.port, function () { return console.log("The Server start on the port " + common_1.default.port); });
